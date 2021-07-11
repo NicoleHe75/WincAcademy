3134deda4fa42810e4a07b2ee5e466e3c7ee12ca
Nicole He <84014715+NicoleHe75@users.noreply.github.com>
2021-07-11 16:23:13 +0200

    Add files via upload

---
+++ excercise_JS01/index.js
@@ -0,0 +1,6 @@
+//This is my first JavaScript code!
+console.log('Hello Winc Academy');
+
+let firstName='Nicole'
+console.log('Nicole');
+// de name if can niet worden gebruikt want dat is een voorgegeven woord
