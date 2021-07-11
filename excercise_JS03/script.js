2f1798dde2f7af85b57bd37fbf8116b202ee485e
Nicole He <84014715+NicoleHe75@users.noreply.github.com>
2021-07-11 21:01:20 +0200

    Add files via upload

---
+++ excercise_JS03/script.js
@@ -0,0 +1,21 @@
+const age = 25;
+if (age > 18) {
+    console.log("Welkom, je mag naar binnen.");
+} else {
+    console.log("Sorry, maar je bent nog te jong.");
+}
+
+const isFemale = true;
+if (isFemale) {
+    console.log("Veel plezier bij Ladies Night!")
+} else {
+    console.log("Maar... de mannen mogen aan de bar blijven wachten!")
+}
+
+
+const driverStatus = bob;
+if (driverStatus = bob) {
+    console.log("Het is veilig om te rijden. Wel thuis!")
+} else {
+    console.log("ik bel graag een taxi voor je.")
+}
